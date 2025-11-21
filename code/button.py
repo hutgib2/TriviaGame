06886ab6a144e0a092287c0c20217e44cb76ce4c
Hugo Smith <hugowsmith1301@gmail.com>
2025-11-21 12:32:20 +0000

    added button click event

--- code/button.py
+++ code/button.py
@@ -5,4 +5,13 @@ class Button(pygame.sprite.Sprite):
         super().__init__()
         self.image = pygame.transform.smoothscale(surf, size)
         self.rect = self.image.get_frect(center=pos)
+
+        # text
         self.text = text
+        self.font = pygame.font.Font(None, 128)
+        self.text_surf = self.font.render(text, True, "darkcyan")
+        self.text_rect = self.text_surf.get_frect(center=pos)
+    
+    def update(self, screen):
+        screen.blit(self.image, self.rect)
+        screen.blit(self.text_surf, self.text_rect)
--- code/main.py
+++ code/main.py
@@ -10,18 +10,22 @@ class TriviaGame():
         self.surfs = folder_importer('assets', 'images')
 
         self.surfs['background'] = pygame.transform.smoothscale(self.surfs['background'], (WINDOW_WIDTH, WINDOW_HEIGHT))
-        self.screen.blit(self.surfs['background'], (0,0))
-        
-        self.button = Button(self.surfs['button'], (WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2), (360, 250), "Play")
-        self.screen.blit(self.button.image, self.button.rect.center)
-        
-        pygame.display.update()
+        self.buttons = pygame.sprite.Group()
+        self.buttons.add(Button(self.surfs['button'], (WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2), (450, 160), "Play"))
 
     def run(self):
         while self.running:
             for event in pygame.event.get():
+                if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+                    for button in self.buttons:
+                        if button.rect.collidepoint(event.pos):
+                            button.kill()
                 if event.type == pygame.QUIT:
                     self.running = False
+            
+            self.screen.blit(self.surfs['background'], (0,0))
+            self.buttons.update(self.screen)
+            pygame.display.update()
                     
 game = TriviaGame()
 game.run()
