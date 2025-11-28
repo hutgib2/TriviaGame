809300453ba1d839049f0598067e9672125a18ba
Hugo Smith <hugowsmith1301@gmail.com>
2025-11-28 11:02:50 +0000

    added game over screen and blank background

--- code/main.py
+++ code/main.py
@@ -10,20 +10,35 @@ class TriviaGame():
         self.screen = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
         self.running = True
         self.surfs = folder_importer('assets', 'images')
-        self.surfs['background'] = pygame.transform.smoothscale(self.surfs['background'], (WINDOW_WIDTH, WINDOW_HEIGHT))
+        self.surfs['home_screen'] = pygame.transform.smoothscale(self.surfs['home_screen'], (WINDOW_WIDTH, WINDOW_HEIGHT))
+        self.surfs['blank_screen'] = pygame.transform.smoothscale(self.surfs['blank_screen'], (WINDOW_WIDTH, WINDOW_HEIGHT))
+        self.surfs['ya_lose'] = pygame.transform.smoothscale(self.surfs['ya_lose'], (WINDOW_WIDTH, WINDOW_HEIGHT))
         self.buttons = pygame.sprite.Group()
         self.start_button = Button(self.surfs['button'], self.surfs['button_hover'], (WINDOW_WIDTH / 1.67, WINDOW_HEIGHT / 1.06), (412, 144), "Play", self.start)
         self.buttons.add(self.start_button)
         self.current_question = None
         self.import_questions()
+        self.background = self.surfs['home_screen']
+        self.choice = None
 
     def start(self):
+        self.background = self.surfs['blank_screen']
         self.start_button.kill()
         self.current_question = self.easy_questions[0]
-        self.buttons.add(Button(self.surfs['button'], self.surfs['button_hover'], (WINDOW_WIDTH / 4, 5*WINDOW_HEIGHT / 8), (1000, 320), self.current_question["correct_answer"], self.start))
-        self.buttons.add(Button(self.surfs['button'], self.surfs['button_hover'], (3*WINDOW_WIDTH / 4, 5*WINDOW_HEIGHT / 8), (1000, 320), self.current_question["incorrect_answers"][0], self.start))
-        self.buttons.add(Button(self.surfs['button'], self.surfs['button_hover'], (WINDOW_WIDTH / 4, 7*WINDOW_HEIGHT / 8), (1000, 320), self.current_question["incorrect_answers"][1], self.start))
-        self.buttons.add(Button(self.surfs['button'], self.surfs['button_hover'], (3*WINDOW_WIDTH / 4, 7*WINDOW_HEIGHT / 8), (1000, 320), self.current_question["incorrect_answers"][2], self.start))
+        rand_choices = [self.current_question["correct_answer"], self.current_question["incorrect_answers"][0], self.current_question["incorrect_answers"][1], self.current_question["incorrect_answers"][2]]
+        random.shuffle(rand_choices)
+        self.buttons.add(Button(self.surfs['button'], self.surfs['button_hover'], (WINDOW_WIDTH / 4, 5*WINDOW_HEIGHT / 8), (1000, 320), rand_choices[0], self.check_result))
+        self.buttons.add(Button(self.surfs['button'], self.surfs['button_hover'], (3*WINDOW_WIDTH / 4, 5*WINDOW_HEIGHT / 8), (1000, 320), rand_choices[1], self.check_result))
+        self.buttons.add(Button(self.surfs['button'], self.surfs['button_hover'], (WINDOW_WIDTH / 4, 7*WINDOW_HEIGHT / 8), (1000, 320), rand_choices[2], self.check_result))
+        self.buttons.add(Button(self.surfs['button'], self.surfs['button_hover'], (3*WINDOW_WIDTH / 4, 7*WINDOW_HEIGHT / 8), (1000, 320), rand_choices[3], self.check_result))
+
+    def check_result(self):
+        if self.choice.text == self.current_question["correct_answer"]:
+            pass
+        else:
+            self.background = self.surfs['ya_lose']
+            self.buttons.empty()
+            self.current_question = None
         
     def display_question(self):
         if self.current_question:
@@ -52,15 +67,16 @@ class TriviaGame():
 
     def run(self):
         while self.running:
-            for event in pygame.event.get():
+            for event in pygame.event.get(): 
                 if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
                     for button in self.buttons:
                         if button.rect.collidepoint(event.pos):
+                            self.choice = button
                             button.clicked()
                 if event.type == pygame.QUIT:
                     self.running = False
             
-            self.screen.blit(self.surfs['background'], (0,0))
+            self.screen.blit(self.background, (0,0))
             self.display_question()
             self.buttons.update(self.screen)
             pygame.display.update()
